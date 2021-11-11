ce7d045d42f8335149a12b5800a48d6e0e8d5188
Jamshid NABIYEV <nabiyevdev@gmail.com>
2021-11-11 15:31:50 +0500

    jadvaluz1

--- data/config.py
+++
@@ -1,4 +0,0 @@
-BOT_TOKEN = '2031259631:AAGBlZX9oPr1F5b0QHzK8QQbQVaatwfy5Eg'    #Test7749
-# BOT_TOKEN = '2132372355:AAESaBehVouCnYSQZD76akZmgtv3zJoCva8'    #JadvalUzbot
-ADMINS = [1589351394]
-IP = 'localhost'
